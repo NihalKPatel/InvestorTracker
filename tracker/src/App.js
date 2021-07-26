45a9d8761165401247e24428063919720ab1fa05
Nihal <70577135+NihalKPatel@users.noreply.github.com>
2021-07-26 14:45:57 +1200

    Setup

--- tracker/src/App.js
+++ tracker/src/App.js
@@ -1,20 +1,21 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {About, Contact, Footer, Home, Navigation} from "./components";
 
-import React, { Component } from 'react';
-import Header from './components/header';
-
-
-
-
-class App extends Component {
-
-    render() {
-        return (
-            <div>
-                <Header />
-
-            </div>
-        );
-    }
+function App() {
+    return (
+        <div className="App">
+            <Router>
+                <Navigation/>
+                <Switch>
+                    <Route path="/" exact component={() => <Home/>}/>
+                    <Route path="/about" exact component={() => <About/>}/>
+                    <Route path="/contact" exact component={() => <Contact/>}/>
+                </Switch>
+                <Footer/>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
